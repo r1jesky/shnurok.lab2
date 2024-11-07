66cbf39aeb52d95ad5a759bcb28a478f5d8b937b
r1jesky <gggggbgfrhxt@gmail.com>
2024-11-07 11:36:03 +0400

    Initial commit

---
+++ shnurok.lab2/src/test/java/ru/ssau/tk/shnurok/lab2/shnurok/lab2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.shnurok.lab2.shnurok.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
